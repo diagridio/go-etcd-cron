afa9aba05be27a8b1dae19a1217c25041e125deb
Cassandra Coyle <cassie@diagrid.io>
2024-12-04 17:05:45 -0600

    add leadership test in cron suite
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

---
+++ tests/suite/leadership_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2024 Diagrid Inc.
+Licensed under the MIT License.
+*/
+
+package suite
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/diagridio/go-etcd-cron/internal/api/stored"
+	"github.com/diagridio/go-etcd-cron/tests/framework/cron"
+	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/protobuf/proto"
+)
+
+func Test_single_instance_leadership(t *testing.T) {
+	t.Parallel()
+
+	cr := cron.SinglePartitionRun(t)
+	defer cr.Stop(t)
+
+	leaderBytes, err := proto.Marshal(&stored.Leadership{
+		Total: 1,
+	})
+	require.NoError(t, err)
+
+	_, err = cr.KV.Do(context.Background(), clientv3.OpPut("abc/leadership/0", string(leaderBytes)))
+	require.NoError(t, err, "Failed to write leadership key with one instance")
+
+	time.Sleep(time.Second)
+
+	resp, err := cr.KV.Do(context.Background(), clientv3.OpGet("abc/leadership/0"))
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+}
+
+func Test_three_instances_leadership(t *testing.T) {
+	t.Parallel()
+
+	cluster := cron.TripplePartitionRun(t)
+	defer func() {
+		for _, cr := range cluster.Crons {
+			cr.Stop(t)
+		}
+	}()
+
+	leaderBytes, err := proto.Marshal(&stored.Leadership{
+		Total: 3,
+	})
+	require.NoError(t, err)
+
+	for i, cr := range cluster.Crons {
+		_, err := cr.KV.Do(context.Background(), clientv3.OpPut(fmt.Sprintf("abc/leadership/%d", i), string(leaderBytes)))
+		require.NoError(t, err, "Failed to write leadership key for instance %d", i)
+	}
+
+	time.Sleep(time.Second)
+
+	// Ensure leadership key is active
+	for i, cr := range cluster.Crons {
+		resp, err := cr.KV.Do(context.Background(), clientv3.OpGet(fmt.Sprintf("abc/leadership/%d", i)))
+		require.NoError(t, err, "Failed to retrieve leadership key for instance %d", i)
+		require.NotNil(t, resp, "Leadership key for instance %d should exist", i)
+	}
+}
