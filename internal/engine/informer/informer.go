984dac533d950614f07012fccffbfc3c4d11e305
Josh van Leeuwen <me@joshvanl.dev>
2026-02-13 17:17:25 +0000

    Informer: retry failed sync base until successful (#107)
    * Informer: retry failed sync base until successful
    
    Prevent the entire cron from shutting down in the event that the base
    syner failed, likely due to transient compaction errors. Instead, log
    the error and retry the sync base after 1 second. Still respect the
    context cancellation, so that the cron can be stopped if needed.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Ignore base sync's which are not in the partition
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- internal/engine/informer/informer.go
+++ internal/engine/informer/informer.go
@@ -10,8 +10,10 @@ import (
 	"errors"
 	"fmt"
 	"sync/atomic"
+	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/mirror"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/diagridio/go-etcd-cron/internal/api/queue"
@@ -69,35 +71,33 @@ func (i *Informer) Run(ctx context.Context) error {
 		return errors.New("informer already running")
 	}
 
-	syncer := newSyncer(i.client, i.key.JobNamespace(), 0)
-	respCh, errCh := syncer.SyncBase(ctx)
-	for resp := range respCh {
-		for _, ev := range resp.Kvs {
-			event, err := i.handleEvent(&clientv3.Event{
-				Type: clientv3.EventTypePut,
-				Kv:   ev,
-			})
-			if err != nil {
-				return fmt.Errorf("failed to handle base sync informer event: %w", err)
-			}
+	var syncer mirror.Syncer
+	var events []*queue.Informed
+	var err error
+	for {
+		syncer, events, err = i.getSyncBase(ctx)
+		if err == nil {
+			break
+		}
 
-			if event == nil {
-				continue
-			}
+		if ctx.Err() != nil {
+			return err
+		}
 
-			for _, event := range event {
-				select {
-				case i.ch <- event:
-				case <-ctx.Done():
-					return ctx.Err()
-				}
-			}
+		i.log.Error(err, "Failed to get base sync from informer, retrying.")
+		select {
+		case <-time.After(time.Second):
+		case <-ctx.Done():
+			return ctx.Err()
 		}
 	}
 
-	err := <-errCh
-	if err != nil {
-		return err
+	for _, ev := range events {
+		select {
+		case i.ch <- ev:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
 	}
 
 	close(i.readyCh)
@@ -109,7 +109,7 @@ func (i *Informer) Run(ctx context.Context) error {
 			return nil
 		case evs := <-ch:
 			for _, ev := range evs.Events {
-				events, err := i.handleEvent(ev)
+				events, err := i.handleEvent(ev, false)
 				if err != nil {
 					i.log.Error(err, "Failed to handle informer event, backing out to rebuild queue.")
 					return nil
@@ -131,7 +131,29 @@ func (i *Informer) Run(ctx context.Context) error {
 	}
 }
 
-func (i *Informer) handleEvent(ev *clientv3.Event) ([]*queue.Informed, error) {
+func (i *Informer) getSyncBase(ctx context.Context) (mirror.Syncer, []*queue.Informed, error) {
+	var events []*queue.Informed
+
+	syncer := newSyncer(i.client, i.key.JobNamespace(), 0)
+	respCh, errCh := syncer.SyncBase(ctx)
+	for resp := range respCh {
+		for _, ev := range resp.Kvs {
+			event, err := i.handleEvent(&clientv3.Event{
+				Type: clientv3.EventTypePut,
+				Kv:   ev,
+			}, true)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			events = append(events, event...)
+		}
+	}
+
+	return syncer, events, <-errCh
+}
+
+func (i *Informer) handleEvent(ev *clientv3.Event, base bool) ([]*queue.Informed, error) {
 	switch ev.Type {
 	case clientv3.EventTypePut:
 		var job stored.Job
@@ -166,9 +188,9 @@ func (i *Informer) handleEvent(ev *clientv3.Event) ([]*queue.Informed, error) {
 			}}, nil
 		}
 
-		// If the event is a creation and the job is not managed by this partition,
-		// we can ignore it.
-		if ev.IsCreate() {
+		// If the event is a creation or we are doing the base sync and the job is
+		// not managed by this partition, we can ignore it.
+		if base || ev.IsCreate() {
 			return nil, nil
 		}
 
